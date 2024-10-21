e85039666f74e465e906a031a81ce2957341c2e2
lorsothy <lorsothy954@gmail.com>
2024-10-21 17:04:22 +0700

    fix product

--- Http/controllers/admin/product/store.php
+++ Http/controllers/admin/product/store.php
@@ -7,12 +7,11 @@ $db = app(Database::class);
 
 $form = ProductForm::validate($_POST);
 
-$db->query("INSERT INTO products (title, slug, description, category_id, available, price, image,created_at,updated_at) VALUES (:title, :slug, :description, :category_id, :available, :price, :image,NOW(),NOW())", [
+$db->query("INSERT INTO products (title, slug, description, category_id, price, image,created_at,updated_at) VALUES (:title, :slug, :description, :category_id, :price, :image,NOW(),NOW())", [
     'title' => $form->attributes['title'],
     'slug' => $form->attributes['slug'],
     'description' => $form->attributes['description'],
     'category_id' => $form->attributes['category_id'],
-    'available' => (bool)$form->attributes['available'],
     'price' => $form->attributes['price'],
     'image' => $form->attributes['attachment'] ?? null
 ]);
