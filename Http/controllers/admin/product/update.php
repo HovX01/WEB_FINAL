c8dba49ee6483ef8b765d2fda7241d31b0d36916
lorsothy <lorsothy954@gmail.com>
2024-10-21 19:17:27 +0700

    fix

--- Http/controllers/admin/product/update.php
+++ Http/controllers/admin/product/update.php
@@ -14,13 +14,12 @@ $form = ProductForm::validate([
     ...$_POST
 ]);
 
-$db->query("UPDATE products SET title = :title, slug = :slug, description = :description, category_id = :category_id, available = :available, price = :price, image = :image WHERE id = :id", [
+$db->query("UPDATE products SET title = :title, slug = :slug, description = :description, category_id = :category_id, price = :price, image = :image WHERE id = :id", [
     'id' => $form->attributes['id'],
     'title' => $form->attributes['title'],
     'slug' => $form->attributes['slug'],
     'description' => $form->attributes['description'],
     'category_id' => $form->attributes['category_id'],
-    'available' => $form->attributes['available'],
     'price' => $form->attributes['price'],
     'image' => $form->attributes['attachment'] ?? null,
 ]);
